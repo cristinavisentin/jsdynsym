c397e35265b4f1e91ce023ddc28ef2fc82199b92
Eric Medvet <eric.medvet@gmail.com>
2025-10-26 19:17:36 +0100

    fix: add missing inherited interface

--- io.github.ericmedvet.jsdynsym.core/src/main/java/io/github/ericmedvet/jsdynsym/core/numerical/NumericalTimeInvariantStatelessSystem.java
+++ io.github.ericmedvet.jsdynsym.core/src/main/java/io/github/ericmedvet/jsdynsym/core/numerical/NumericalTimeInvariantStatelessSystem.java
@@ -23,4 +23,4 @@ package io.github.ericmedvet.jsdynsym.core.numerical;
 import io.github.ericmedvet.jsdynsym.core.StatelessSystem;
 import io.github.ericmedvet.jsdynsym.core.TimeInvariantStatelessSystem;
 
-public interface NumericalTimeInvariantStatelessSystem extends NumericalDynamicalSystem<StatelessSystem.State>, TimeInvariantStatelessSystem<double[], double[]> {}
+public interface NumericalTimeInvariantStatelessSystem extends NumericalDynamicalSystem<StatelessSystem.State>, TimeInvariantStatelessSystem<double[], double[]>, NumericalStatelessSystem {}
