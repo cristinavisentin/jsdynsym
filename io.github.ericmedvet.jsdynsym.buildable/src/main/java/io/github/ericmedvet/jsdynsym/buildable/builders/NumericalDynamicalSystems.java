5d7c7a819020482b6e4fe85a7f3164c937897d69
Eric Medvet <eric.medvet@gmail.com>
2025-12-10 18:38:00 +0100

    chore: update and clean license

--- io.github.ericmedvet.jsdynsym.buildable/src/main/java/io/github/ericmedvet/jsdynsym/buildable/builders/NumericalDynamicalSystems.java
+++ io.github.ericmedvet.jsdynsym.buildable/src/main/java/io/github/ericmedvet/jsdynsym/buildable/builders/NumericalDynamicalSystems.java
@@ -17,21 +17,6 @@
  * limitations under the License.
  * =========================LICENSE_END==================================
  */
-/*
- * Copyright 2025 eric
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package io.github.ericmedvet.jsdynsym.buildable.builders;
 
@@ -42,7 +27,12 @@ import io.github.ericmedvet.jnb.datastructure.DoubleRange;
 import io.github.ericmedvet.jsdynsym.core.composed.InStepped;
 import io.github.ericmedvet.jsdynsym.core.composed.OutStepped;
 import io.github.ericmedvet.jsdynsym.core.composed.Stepped;
-import io.github.ericmedvet.jsdynsym.core.numerical.*;
+import io.github.ericmedvet.jsdynsym.core.numerical.EnhancedInput;
+import io.github.ericmedvet.jsdynsym.core.numerical.LinearCombination;
+import io.github.ericmedvet.jsdynsym.core.numerical.MultivariateRealFunction;
+import io.github.ericmedvet.jsdynsym.core.numerical.Noised;
+import io.github.ericmedvet.jsdynsym.core.numerical.NumericalDynamicalSystem;
+import io.github.ericmedvet.jsdynsym.core.numerical.Sinusoidal;
 import io.github.ericmedvet.jsdynsym.core.numerical.ann.DelayedRecurrentNetwork;
 import io.github.ericmedvet.jsdynsym.core.numerical.ann.HebbianMultiLayerPerceptron;
 import io.github.ericmedvet.jsdynsym.core.numerical.ann.MultiLayerPerceptron;
